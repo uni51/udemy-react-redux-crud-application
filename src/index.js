b344685da5206a8ad5e8a2ed178bcb7dddfe9efe
uni51 <uni51@users.noreply.github.com>
2018-07-28 16:27:25 +0900

    add rdeux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   // Providerコンポーネントでルートコンポーネントをラップすることで、その配下にぶら下がる要素に対してもStoreを渡せる
