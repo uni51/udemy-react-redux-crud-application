e3cbdb59f19865de5c2b548e8afb5a45421e21a5
uni51 <uni51@users.noreply.github.com>
2018-07-28 16:04:08 +0900

    implement events index

---
+++ src/components/events_index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import _ from 'lodash'
+
+import { readEvents } from '../actions'
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+// 現在のRedux上のStore情報(State)を、コンポーネントへ渡す
+const mapStateToProps = state => ({events: state.events})
+// const mapStateToProps = state => {
+//   return { value: state.count.value }
+// }
+
+// Actionsを発行するためのdispatch実行をコールバックとしてコンポーネントに渡す
+const mapDispatchToProps = ({ readEvents })
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
